236a94e96e29af7f607c5a8943caa056751811d4
vokomarov <vovikems@gmail.com>
2025-11-11 09:31:29 +0200

    Add manual relay override button

--- src/RELAY.h
+++ src/RELAY.h
@@ -3,19 +3,32 @@
 struct RELAY : Service::Switch {
 
   int relayPin;
+  int statusPin;
+  int overrideBtnPin;
   SpanCharacteristic *power;
 
-  RELAY(int relayPin) : Service::Switch() 
+  RELAY(int relayPin, int statusPin, int overrideBtnPin) : Service::Switch() 
   {
-    power = new Characteristic::On();
+    power = new Characteristic::On(HIGH, true);
     this->relayPin = relayPin;
-    pinMode(relayPin, OUTPUT);
+    this->statusPin = statusPin;
+    this->overrideBtnPin = overrideBtnPin;
     digitalWrite(this->relayPin, HIGH); // turn off relay's Normally Open pad
   }
 
   boolean update() 
   {
+    boolean isStateOverridden = digitalRead(overrideBtnPin) == HIGH;
+    
+    if (isStateOverridden) {
+      return true;
+    }
+
     digitalWrite(relayPin, power->getNewVal() >= 1 ? LOW : HIGH);
+    digitalWrite(statusPin, power->getNewVal() >= 1 ? HIGH : LOW);
+
+    Serial.printf("Relay remotely set to %s\n\r", power->getNewVal() >= 1 ? "ON" : "OFF");
+
     return true;
   }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <HomeSpan.h>
+#include <WiFi.h>
+#include "esp_wifi.h"
 #include "secrets.h"
 #include "RELAY.h"
 
+#define RELAY_PIN GPIO_NUM_5
+#define MANUAL_SW_PIN GPIO_NUM_10
+#define LED_PIN GPIO_NUM_7
+
+bool isRelayOverridden = false;
+void manualBtnPoll();
+unsigned long lastCheckedTime = 0;
+const long interval = 1000; // 1 second
+
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(MANUAL_SW_PIN, INPUT_PULLDOWN);
+  pinMode(RELAY_PIN, OUTPUT);
+
   Serial.begin(115200);
+
+  WiFi.setTxPower(WIFI_POWER_8_5dBm);
+  esp_wifi_set_protocol(WIFI_IF_STA, WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G);
 
   homeSpan.setPairingCode("11122333");
   homeSpan.begin(Category::Switches, "Gas Boiler Switch");
@@ -15,9 +33,32 @@ void setup() {
   new SpanAccessory();
     new Service::AccessoryInformation();    
       new Characteristic::Identify();                       
-    new RELAY(GPIO_NUM_5);
+    new RELAY(RELAY_PIN, LED_PIN, MANUAL_SW_PIN);
 }
 
 void loop() {
   homeSpan.poll();
+
+  unsigned long currentTime = millis();
+  if (currentTime - lastCheckedTime >= interval) {
+    lastCheckedTime = currentTime;
+    manualBtnPoll();
+  }
+}
+
+void manualBtnPoll() {
+  boolean isManualBtnEnabled = digitalRead(MANUAL_SW_PIN) == HIGH;
+  boolean isRelayEnabled = digitalRead(RELAY_PIN) == LOW;
+
+  if (!isRelayOverridden && isManualBtnEnabled) {
+    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(RELAY_PIN, LOW);
+    isRelayOverridden = true;
+    Serial.println("Relay manually overridden to ON");
+  } else if (!isManualBtnEnabled && isRelayOverridden && isRelayEnabled) {
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(RELAY_PIN, HIGH);
+    isRelayOverridden = false;
+    Serial.println("Relay manually overridden to OFF");
+  }
 }
